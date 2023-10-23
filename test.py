00de951380e751ccd8925bf7a522ed90e027e7df
Abhishek Kaushikkar <abhikaushikkar@gmail.com>
2023-10-23 16:58:54 -0700

    requests working to weather api

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
 import requests
+import json
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_180m,winddirection_180m&timezone=America%2FNew_York")
+def get_wind(lat, long, actual_altitude):
+    possible_altitudes = [10,80,120,180]
+    differences = []
+    for altitude in possible_altitudes:
+        diff =  abs(actual_altitude-altitude)
+        differences.append(diff)
 
-print(response.json())
+    new_alt = possible_altitudes[differences.index(min(differences))] 
+    print(new_alt)
+
+    request_url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=windspeed_{alt},winddirection_{alt}&timezone=America%2FNew_York".format(latitude = str(lat),longitude = str(long), alt = str(str(new_alt)+"m"))
+
+    
+    response = requests.get(request_url)
+    
+    #answer = json.loads(response.json())
+    answer = response.json()
+    print(answer)
+
+    wind_speed = answer["windspeed_120m"]
+    #wind_direction = answer[str("winddirection_{a}m".format(a = new_alt))]
+
+    return wind_speed
+
+    
+    
+
+
+val = get_wind(52.52,13.419998, 101)
+print(val)
