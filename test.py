a85041dded80839c3629b2879c3dcfef1a0addaf
Abhishek Kaushikkar <abhikaushikkar@gmail.com>
2023-10-23 16:29:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-print("test")
-print("hi")
-print("no")
-print("new")
+
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_180m,winddirection_180m&timezone=America%2FNew_York")
+
+print(response.json())
