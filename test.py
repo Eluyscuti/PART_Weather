07a184700660bf6f8c9961df49089e64a3c67bff
Abhishek Kaushikkar <abhikaushikkar@gmail.com>
2023-10-24 08:07:19 -0700

    get current weather

--- test.py
+++ test.py
@@ -11,19 +11,21 @@ def get_wind(lat, long, actual_altitude):
     new_alt = possible_altitudes[differences.index(min(differences))] 
     print(new_alt)
 
-    request_url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=windspeed_{alt},winddirection_{alt}&timezone=America%2FNew_York".format(latitude = str(lat),longitude = str(long), alt = str(str(new_alt)+"m"))
-
+    #request_url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=windspeed_{alt},winddirection_{alt}&timezone=America%2FNew_York".format(latitude = str(lat),longitude = str(long), alt = str(str(new_alt)+"m"))
+    request_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=windspeed_10m,winddirection_10m"
     
     response = requests.get(request_url)
     
     #answer = json.loads(response.json())
     answer = response.json()
-    print(answer)
+    #print(answer)
+    #print(answer["latitude"])
 
-    wind_speed = answer["windspeed_120m"]
-    #wind_direction = answer[str("winddirection_{a}m".format(a = new_alt))]
+    wind_speed = answer["current"]["windspeed_{alt}m".format(alt = str(10))]
+    
+    wind_direction = answer["current"][str("winddirection_{a}m".format(a = 10))]
 
-    return wind_speed
+    return wind_speed, wind_direction
 
     
     
